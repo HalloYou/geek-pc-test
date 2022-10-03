d11603f20d097c8e5165828013b300822439bf77
YouLi <wujishishen@yahoo.com>
2022-10-03 17:15:54 +0800

    登录模块-添加样式

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,17 @@
 import { Component } from 'react';
-import { Button, DatePicker } from 'antd'
+import { Card } from 'antd'
+import logo from '../../assets/logo.png';
+import "./index.css"
 class Login extends Component {
   constructor(props) {
     super(props);
   }
   state = {}
   render() {
-    return (<div>Layout
-      <Button>add</Button>
-      <DatePicker></DatePicker>
+    return (<div className='login'>
+      <Card className='login-container'>
+        <img className='login-logo' src={logo} alt="" />
+      </Card>
     </div>);
   }
 }
